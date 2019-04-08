a5ebbd8b95d5be1ef9ef0abe231120211f35540e
Nilesh Savani <nileshsavani09@gmail.com>
2019-04-08 07:38:20 -0400

    added two screens of stocks and stock details with graph

---
+++ app/i18n/index.ts
@@ -0,0 +1,20 @@
+import { I18nManager } from "react-native";
+import * as RNLocalize from "react-native-localize";
+import i18n from "i18n-js";
+
+import en from "./locales/en";
+
+i18n.translations = { en };
+i18n.fallbacks = true;
+
+const fallback = { languageTag: "en", isRTL: false };
+const { languageTag, isRTL } =
+  RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) ||
+  fallback;
+
+I18nManager.forceRTL(isRTL); // optional, you might not want to handle RTL
+i18n.locale = languageTag;
+
+export default i18n;
+
+export const t = i18n.t.bind(i18n);
